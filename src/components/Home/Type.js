30eb3b9443898a14aff5cfd65263f1f42257aa7f
AreebRehman5 <136895724+AreebRehman5@users.noreply.github.com>
2024-05-16 04:28:38 +0500

    first commit

---
+++ src/components/Home/Type.js
@@ -0,0 +1,22 @@
+import React from "react";
+import Typewriter from "typewriter-effect";
+
+function Type() {
+  return (
+    <Typewriter
+      options={{
+        strings: [
+          "Front-end web development",
+          "Graphics Designing in progress",
+          "Canva",
+          "DATA ENTRY OPERATOR",
+        ],
+        autoStart: true,
+        loop: true,
+        deleteSpeed: 50,
+      }}
+    />
+  );
+}
+
+export default Type;
